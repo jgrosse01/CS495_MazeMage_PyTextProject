7d56a06a1aa4479415603a23d0a3456faea124b0
Jake Grosse <jgrosse@carroll.edu>
2022-01-19 08:18:40 -0700

    Updated text art to include developer and version number

--- text_adventure/Game.py
+++ text_adventure/Game.py
@@ -3,6 +3,13 @@ classImplemented = [True, False, False]         # for easy checking and loading 
 
 availableRooms = []
 GameMap = [9][9]
+# Location in the map by grid coordinates
+playerLocation = (0,0)
+# Cardinal Direction facing, 0 = N, 1 = E, 2 = S, 3 = W
+playerOrientation = 1
+
+helpString = ""
+textArt = ""
 
 
 # forces input to be lower case, ease of use by the programmer
@@ -19,3 +26,15 @@ def processInput(string):
 # displays the current hp and mp as well as the room status
 def displayStatus():
     pass
+
+
+def loadHelpString():
+    pass
+
+
+def loadTextArt():
+    pass
+
+
+if __name__ == '__main__':
+    global helpString, GameMap, playerClasses, classImplemented
